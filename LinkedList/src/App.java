57d5ecf5187d853f7321a35be3388b63b5e48f5b
Ayush Rathore <aayushh79@gmail.com>
2022-05-25 21:27:13 +0530

    Print linked list in the form of array

--- LinkedList/src/App.java
+++ LinkedList/src/App.java
@@ -1,9 +1,14 @@
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) throws Exception {
         var list = new LinkedList();
         list.addLast(1);
         list.addLast(2);
-        list.addFirst(3);
-        System.out.println(list.indexOf(3));
+        list.addLast(3);
+        list.addLast(4);
+        list.addLast(5);
+        var array = list.toArray();
+        System.out.println(Arrays.toString(array));
     }
 }
