14f5b7b4295c3cee080e69bb3f286b068338d64d
ayusshrathore <ayusshrathore@gmail.com>
2022-08-28 00:06:35 +0530

    Update code for bubble sort

--- Sorting/src/BubbleSort.java
+++ Sorting/src/BubbleSort.java
@@ -1,13 +1,13 @@
 public class BubbleSort {
-  public void sort(int[] arr) {
+  public static void sort(int[] arr) {
     boolean isSorted;
     for (int i = 0; i < arr.length; i++) {
       isSorted = true;
       for (int j = 1; j < arr.length - i; j++) {
-        if (arr[i] > arr[j]) {
+        if (arr[j] < arr[j - 1]) {
           int temp = arr[j];
-          arr[i] = arr[j];
-          arr[j] = temp;
+          arr[j] = arr[j - 1];
+          arr[j - 1] = temp;
           isSorted = false;
         }
       }
